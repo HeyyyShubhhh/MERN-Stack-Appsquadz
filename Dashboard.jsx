15f5fde26a6d66308ec333e88ba3fd6dc6bed366
HeyyyShubhhh <prajapatishubh1111@gmail.com>
2026-07-16 13:50:56 +0530

    Add files via upload

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Add from './Add'
 
 const Dashboard = () => {
   return (
-  <td>1</td>
-  <td>Learn Html 5</td>
-  
+    <>
+    <Add/>
+    <div className="container">
+        <table className='table text-center'>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>TASK NAME</th>
+                    <th>ACTION</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Learn HTML</td>
+                    <td>
+                        <button className='btn btn-warning'>Edit</button>
+                        <button className='btn btn-danger mx-4'>Delete</button>
+
+                    </td>
+                </tr>
+            </tbody>
+            </table>
+    </div>
+    </>
   )
 }
 
